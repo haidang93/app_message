6566058243f725c43a72995bee129a227575222b
Nguyen Hai Dang <dannynguyen.vnn@gmail.com>
2025-07-22 13:19:38 +0700

    update

--- lib/app_message.dart
+++ lib/app_message.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-part 'common/enum.dart';
+part 'common/app_message_type_model.dart';
 part 'common/functions.dart';
 part 'widget/views.dart';
 part 'widget/inkwell.dart';
